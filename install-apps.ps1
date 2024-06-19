4af87d981055d8b601be1e93da018022cb535cdc
Jeroen-scripts <85566291+Jeroen-scripts@users.noreply.github.com>
2024-06-19 16:22:41 +0200

    Update install-apps.ps1

--- install-apps.ps1
+++ install-apps.ps1
@@ -5,23 +5,24 @@
 # Omnitracker Client 
 
 #region Set logging 
-$logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
+$logFile = "c:\installlogs\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
 function Write-Log {
     Param($message)
     Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
 }
 #endregion
 
-#download software repo
-c:\\temp\\azcopy.exe copy 'https://aibsoftwarerepository.blob.core.windows.net/softwaresource/software.zip?sp=r&st=2023-08-16T14:22:43Z&se=2023-08-30T22:22:43Z&spr=https&sv=2022-11-02&sr=b&sig=aB8m1nc%2Bzz1sPGk%2FKOHZ8DMkcGPcykWIs6%2B6ph5q8mw%3D' c:\\temp\\software.zip
-Expand-Archive 'c:\\temp\\software.zip' c:\\temp
-#endregion
-
 #region Remote Desktop Manager
 try {
     Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\software\RDM\Setup.RemoteDesktopManager.2021.1.44.0.msi', 'TRANSFORMS="C:\temp\software\RDM\RDMSettings.mst"', '/quiet'
     if (Test-Path "C:\Program Files (x86)\Devolutions\Remote Desktop Manager\RemoteDesktopManager64.exe") {
-        Write-Log "Remote Desktop Manager has been installed"
+        Write-Log "Remote Desktop Manager has been installed. Creating desktop shortcut"
+        $TargetFile   = "C:\Program Files (x86)\Devolutions\Remote Desktop Manager\RemoteDesktopManager64.exe"
+        $ShortcutFile = "C:\Users\Public\Desktop\rdm.lnk"
+        $WScriptShell = New-Object -ComObject WScript.Shell
+        $Shortcut     = $WScriptShell.CreateShortcut($ShortcutFile)
+        $Shortcut.TargetPath = $TargetFile
+        $Shortcut.Save()
     }
     else {
         write-log "Error locating the Remote Desktop Manager executable"
@@ -29,30 +30,37 @@ try {
 }
 catch {
     $ErrorMessage = $_.Exception.message
-    write-log "Error installing Remote Desktop Manager: $ErrorMessage"
+    write-log "Error occured installing Remote Desktop Manager: $ErrorMessage"
 }
 #endregion
 
-#region Remote Desktop Manager
+#region Omnitracker
 try {
-    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\software\OT\OT_12_0_0_x64.msi', 'TRANSFORMS="C:\temp\software\OT\Omninet Omnitracker 12.0.0.10344 x64 EN R01.mst"', '/quiet'
+    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\software\OT\OT.msi', 'TRANSFORMS="C:\temp\software\OT\OT.mst"', '/quiet'
     if (Test-Path "C:\Program Files\OMNITRACKER\OMNINET.OMNITRACKER.Client.exe") {
-        Write-Log "Omnitracker Client has been installed"
+        Write-Log "Omnitracker Client has been installed. Creating desktop shortcut"
+        $TargetFile   = "C:\Program Files\OMNITRACKER\OMNINET.OMNITRACKER.Client.exe"
+        $ShortcutFile = "C:\Users\Public\Desktop\omnitracker.lnk"
+        $WScriptShell = New-Object -ComObject WScript.Shell
+        $Shortcut     = $WScriptShell.CreateShortcut($ShortcutFile)
+        $Shortcut.TargetPath = $TargetFile
+        $Shortcut.Save()
     }
     else {
+        $ErrorMessage = $_.Exception.message
         write-log "Error locating the Omnitracker Client executable"
     }
 }
 catch {
     $ErrorMessage = $_.Exception.message
-    write-log "Error installing Omnitracker Client: $ErrorMessage"
+    write-log "Error occured installing Omnitracker Client: $ErrorMessage"
 }
 #endregion
 
 #region cleanup
 try {
-    Remove-Item -Path "C:\temp\software" -Recurse -Force
-    if (Test-Path "C:\temp\software") {
+    Remove-Item -Path "C:\temp\*" -Recurse -Force
+    if (!(Test-Path "C:\temp\software")) {
         Write-Log "Software source folder failed to be removed"
     }
     else {
@@ -63,17 +71,4 @@ catch {
     $ErrorMessage = $_.Exception.message
     write-log "Error removing software source folder: $ErrorMessage"
 }
-
-try {
-    Remove-Item -Path "C:\temp\software.zip" -Force
-    if (Test-Path "C:\temp\software.zip") {
-        Write-Log "Software zip file failed to be removed"
-    }
-    else {
-        write-log "Error removing software zip file"
-    }
-}
-catch {
-    $ErrorMessage = $_.Exception.message
-    write-log "Error removing software zip file: $ErrorMessage"
-}
+#endregion
